9630205321a56c5c1f937489618af43e0c36ada0
unknown <christian.bugan@gmail.com>
2023-07-24 11:29:32 +0300

    curs15

---
+++ src/main/java/curs15/DBQuerys.java
@@ -0,0 +1,35 @@
+package curs15;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class DBQuerys {
+
+	public static List<String> dbSelectQuery(Connection conn, String query) {
+		List<String> resultList = new ArrayList<String>();
+		
+		try {
+			Statement statement = conn.createStatement();
+			ResultSet result = statement.executeQuery(query);
+			
+			while(result.next()) {
+				resultList.add(result.getString(2));
+			}
+			
+			
+		}catch(SQLException e) {
+			System.out.println("Nu am putut executa query-ul!");
+		}
+		
+		
+		return resultList;
+		
+	}
+	
+	
+}
